940279a116291325a865bc05d27d8ad24b043e71
PioneerX7 <vita-kopy@yandex.ru>
2023-04-19 14:42:31 +0300

    метод удаления сотрудника

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,51 @@
 import java.sql.*;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
 
+        final String user = "postgres";
+        final String password = "Strahov1488";
+        final String url = "jdbc:postgresql://localhost:5432/db_skypro";
+
+        try (final Connection connection =
+                     DriverManager.getConnection(url, user, password);
+
+             PreparedStatement statement =
+                     connection.prepareStatement("SELECT * FROM employee")) {
+
+            ResultSet resultSet = statement.executeQuery();
+
+
+            while (resultSet.next()) {
+
+                int idOfPerson = resultSet.getInt("id");
+                System.out.println("ID работника: " + idOfPerson);
+
+                String namePerson = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                String gender = resultSet.getString("gender");
+                String city = resultSet.getString("city_id");
+
+                System.out.println(namePerson + " " + lastName + " " + gender + " Город: " +
+                        city);
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Ошибка при подключении к БД!");
+            e.printStackTrace();
+        }
+        EmployeeDAO employeeDAO = new EmployeeDAOImpl();
+        List<Employee> employees = employeeDAO.getAllEmployee();
+        for (Employee employee : employees){
+            System.out.println(employee.getFirst_name());
+        }
+
+        employeeDAO.deleteEmployee(1);
+        for (Employee employee : employees){
+            System.out.println(employee.getFirst_name());
+        }
     }
 }
+
---
+++ src/main/java/EmployeeDAO.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public interface EmployeeDAO {
+    List<Employee> getAllEmployee();
+    Employee getEmployeeByID(int id);
+    void createEmployee(Employee employee);
+    void updateEmployee(Employee employee);
+    void deleteEmployee(int id);
+}
